ef688af674dc3190ec4007de26d283003c81822b
bigazzi96 <fbigazzi96@gmail.com>
2024-05-07 18:27:46 +0200

    added content js

---
+++ script.js
@@ -0,0 +1,12 @@
+const nome = prompt('scrivi il tuo nome');
+const cognome = prompt('scrivi il tuo cognome');
+const colore = prompt('scrivi il tuo colore preferito');
+
+
+console.log(nome + cognome + colore);
+
+document.getElementById('output').innerHTML = nome+cognome+colore;
+
+
+
+
